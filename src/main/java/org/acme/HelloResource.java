9b5bd3004a6fa38bf0b78377baa2b00bac4c0b4b
komuro.hiraku <komuro.hiraku@classmethod.jp>
2021-01-15 23:31:25 +0900

    Add Hello Template
    - feat: Add Template Engine dependency
    - feat: Add Hello Template

---
+++ src/main/java/org/acme/HelloResource.java
@@ -0,0 +1,25 @@
+package org.acme;
+
+import io.quarkus.qute.Template;
+import io.quarkus.qute.TemplateInstance;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class HelloResource {
+
+    @Inject
+    Template hello;
+
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    public TemplateInstance get(@QueryParam("name") String name) {
+        // テンプレートにInjection
+        return hello.data("name", name);
+    }
+}
